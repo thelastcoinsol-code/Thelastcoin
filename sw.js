fb12b9b4c7ff20453026b599dbde59afe27d75da
thelastcoinsol-code <thelastcoinsol@gmail.com>
2026-08-20 22:52:30 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="solana-scanner-final-v1";const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))});
